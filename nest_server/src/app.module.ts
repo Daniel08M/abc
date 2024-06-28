f7c56ec508479334a2612446d35200fb90d52218
stephan <stephan102002@gmail.com>
2024-06-27 22:06:46 -0500

    resources for modules

--- nest_server/src/app.module.ts
+++ nest_server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './modulos/usuarios/usuarios.module';
+import { JuegosModule } from './modulos/juegos/juegos.module';
+import { EquiposModule } from './modulos/equipos/equipos.module';
 
 @Module({
-  imports: [],
+  imports: [UsuariosModule, JuegosModule, EquiposModule],
   controllers: [AppController],
   providers: [AppService],
 })
